eb0d2c6a7efca1a38265cf0acf5df285c9cb82cb
SrElotero <nobel.blog6@live.com.mx>
2023-03-06 14:21:55 -0800

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,9 +1,32 @@
-num1 = 5
-num2 = -2
+n1 = float(input("Introduce tu primer número: ") )
+n2 = float(input("Introduce tu segundo número: ") )
 
-def suma(numero1, numero2):
-    total = numero1 + numero2
-    return total
+opcion = 0
+while True:
+    print("""
+    Dime, ¿qué quieres hacer?
+    
+    1) Sumar los dos números
+    2) Restar los dos números
+    3) Multiplicar los dos números
+    4) Cambiar los números elegidos
+    5) Apagar calculadora
+    """)
+    opcion = int(input("Elige una opción: ") )     
 
-resultado = suma(num1, num2)
-print('El resultado es: ',resultado)
+    if opcion == 1:
+        print(" ")
+        print("RESULTADO: La suma de",n1,"+",n2,"es igual a",n1+n2)
+    elif opcion == 2:
+        print(" ")
+        print("RESULTADO: La resta de",n1,"-",n2,"es igual a",n1-n2)
+    elif opcion == 3:
+        print(" ")
+        print("RESULTADO: El producto de",n1,"*",n2,"es igual a",n1*n2)
+    elif opcion == 4:
+        n1 = float(input("Introduce tu primer número: ") )
+        n2 = float(input("Introduce tu segundo número: ") )
+    elif opcion == 5:
+        break
+    else:
+        print("Opción incorrecta")
